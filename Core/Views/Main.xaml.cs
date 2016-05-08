831bf27189e6ac98e65ea92020b2ad488157c634
Reclusior <n.peters@23r0.cc>
2016-05-08 18:06:38 +0200

    Added Tabcontrol
    + Tabs are created when opening a file
    + every Tab generates a new Texteditor
    + "Close File" is active, when a file is open
    + "Close File" closes a file
    + added a Message, when no File is open

--- Core/Views/Main.xaml.cs
+++ Core/Views/Main.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿using ICSharpCode.AvalonEdit;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace JAO_PI.Core.Views
 {
@@ -18,8 +24,67 @@ namespace JAO_PI.Core.Views
         }
         private void Create_File_Click(object sender, RoutedEventArgs e)
         {
-            textEditor.Visibility = Visibility.Visible;
-            textEditor.ShowLineNumbers = true;
+            TextEditor text = new TextEditor();
+            text.FontSize = 13;
+            text.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+            text.FontFamily = new FontFamily("Consolas");
+            text.ShowLineNumbers = true;
+            text.Margin = new Thickness(0, 0, 5, 0);
+
+            Grid grid = new Grid();
+            grid.Children.Add(text);
+
+            TabItem tab = new TabItem();
+            tab.Header = "new.pwn";
+            tab.Content = grid;
+
+            tabControl.Items.Add(tab);
+            tabControl.SelectedItem = tab;
+
+            Empty_Message.Visibility = Visibility.Hidden;
+            Empty_Message.IsEnabled = false;
+
+            tabControl.Visibility = Visibility.Visible;            
+        }
+
+        private void Open_File_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "PAWN Files (*.inc, *.pwn)|*.inc;*.pwn|All files (*.*)|*.*";
+            openFileDialog.Title = "Open PAWN File...";
+            openFileDialog.InitialDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            if (openFileDialog.ShowDialog() == true)
+            {
+                TextEditor text = new TextEditor();
+                text.FontSize = 13;
+                text.HorizontalScrollBarVisibility = ScrollBarVisibility.Auto;
+                text.FontFamily = new FontFamily("Consolas");
+                text.ShowLineNumbers = true;
+                text.Text = File.ReadAllText(openFileDialog.FileName, System.Text.Encoding.Default);
+                text.Margin = new Thickness(0, 0, 5, 0);
+
+                Grid grid = new Grid();
+                grid.Children.Add(text);
+
+                TabItem tab = new TabItem();
+                tab.Header = openFileDialog.SafeFileName;
+                tab.Content = grid;
+
+                tabControl.Items.Add(tab);
+                tabControl.SelectedItem = tab;
+
+                Empty_Message.Visibility = Visibility.Hidden;
+                Empty_Message.IsEnabled = false;
+
+                tabControl.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Close_File_Click(object sender, RoutedEventArgs e)
+        {
+            Empty_Message.IsEnabled = true;
+            Close_File.IsEnabled = false;
+            Empty_Message.Visibility = Visibility.Visible;
         }
     }
 }
